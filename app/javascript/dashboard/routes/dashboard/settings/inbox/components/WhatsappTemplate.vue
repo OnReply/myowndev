0cb6fcd60b5f2011e2186957a2f69b6583ed622d
shozab nadeem <shozabnadeem1103@gmail.com>
2023-04-13 03:14:59 +0500

    Display Templates

---
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/WhatsappTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mt-2">
+    <templates-picker
+      :inbox-id="inbox.id"
+      classes="max-h-full template__list-container"
+      :filter-templates="false"
+    />
+  </div>
+</template>
+
+<script>
+import TemplatesPicker from '../../../../../components/widgets/conversation/WhatsappTemplates/TemplatesPicker.vue'
+export default {
+  components: { TemplatesPicker },
+  props: {
+    inbox: {
+      type: Object,
+      default: {},
+    },
+  },
+  computed: {},
+};
+</script>
+<style lang="scss" scoped>
+.template__list-container {
+  background-color: var(--s-25);
+  border-radius: var(--border-radius-medium);
+  overflow-y: auto;
+  padding: var(--space-one);
+}
+</style>
