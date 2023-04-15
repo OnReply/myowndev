6a10920f626023fe3871d75ed916afa3dd00d0bc
shozab nadeem <shozabnadeem1103@gmail.com>
2023-04-15 16:35:09 +0500

    Display values of selected template in builder

--- app/javascript/dashboard/routes/dashboard/settings/inbox/components/WhatsappTemplate.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/WhatsappTemplate.vue
@@ -4,21 +4,57 @@
       :inbox-id="inbox.id"
       classes="max-h-full template__list-container"
       :filter-templates="false"
+      @onSelect="editTemplate"
     />
+    <woot-modal
+      :show.sync="showWhatsAppTemplatesBuilderModal"
+      :on-close="onClose"
+      size="modal-big"
+    >
+      <woot-modal-header
+        :header-title="$t('WHATSAPP_TEMPLATES.MODAL.TITLE')"
+        header-content="Edit your Template"
+      />
+      <div class="row modal-content">
+        <whatsapp-template-builder
+          :inbox-id="inbox.id"
+          :show="showWhatsAppTemplatesBuilderModal"
+          :template="template"
+        />
+      </div>
+    </woot-modal>
   </div>
 </template>
 
 <script>
-import TemplatesPicker from '../../../../../components/widgets/conversation/WhatsappTemplates/TemplatesPicker.vue'
+import TemplatesPicker from '../../../../../components/widgets/conversation/WhatsappTemplates/TemplatesPicker.vue';
+import WhatsappTemplateBuilder from './WhatsappTemplateBuilder.vue';
 export default {
-  components: { TemplatesPicker },
+  components: { TemplatesPicker, WhatsappTemplateBuilder },
   props: {
     inbox: {
       type: Object,
-      default: {},
+      default() {
+        return {};
+      },
     },
   },
+  data() {
+    return {
+      showWhatsAppTemplatesBuilderModal: false,
+      template: null,
+    };
+  },
   computed: {},
+  methods: {
+    editTemplate(template) {
+      this.showWhatsAppTemplatesBuilderModal = true;
+      this.template = template;
+    },
+    onClose() {
+      this.showWhatsAppTemplatesBuilderModal = false;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -27,5 +63,8 @@ export default {
   border-radius: var(--border-radius-medium);
   overflow-y: auto;
   padding: var(--space-one);
+}
+.modal-content {
+  padding: 2.5rem 3.2rem;
 }
 </style>
---
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/WhatsappTemplateBuilder.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="columns small">
+    <div class="input-group-field">
+      <woot-input
+        v-model.trim="template.name"
+        type="text"
+        placeholder="test"
+        label="test"
+      />
+    </div>
+    <div class="input-group-field">
+      <label for="category"> Category </label>
+      <select v-model="template.category" name="category">
+        <option v-for="t in templateTypes" :key="t" :value="t">
+          {{ t }}
+        </option>
+      </select>
+    </div>
+    <div class="input-group-field">
+      <label for="language"> Language </label>
+      <select v-model="template.language" name="language">
+        <option
+          v-for="language in languages"
+          :key="language.code"
+          :value="language.code"
+        >
+          {{ language.language }}
+        </option>
+      </select>
+    </div>
+    <div class="input-group-field">
+      <woot-input
+        v-model.trim="headerValue"
+        type="text"
+        placeholder="Type a string"
+        label="Header"
+      />
+    </div>
+    <div class="input-group-field">
+      <label for="body"> Body </label>
+      <textarea
+        v-model.trim="bodyValue"
+        type="text"
+        name="body"
+        placeholder="Type a string"
+      />
+    </div>
+    <div class="input-group-field">
+      <woot-input
+        v-model.trim="footerValue"
+        type="text"
+        placeholder="Type a string"
+        label="Footer"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import facebookLanguageList from '../../../../../helper/facebookLanguagesList.json'
+export default {
+  components: {
+  },
+  props: {
+    inboxId: {
+      type: Number,
+      default: undefined,
+    },
+    show: {
+      type: Boolean,
+      default: true,
+    },
+    template: {
+      type: Object,
+      default() {
+        return {
+          name: 'test',
+          category: 'UTILITY',
+          language: 'en',
+          components: [],
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      templateTypes: ['UTILITY', 'MARKETING', 'AUTHENTICATION'],
+      languages: facebookLanguageList,
+      bodyValue: '',
+      headerValue: '',
+      footerValue: '',
+    };
+  },
+  mounted() {
+    this.getComponentValues();
+  },
+  methods: {
+    getComponentValues() {
+      this.template.components.forEach(component => {
+        if (component.type === 'HEADER') {
+          this.headerValue = component.text;
+        } else if (component.type === 'BODY') {
+          this.bodyValue = component.text;
+        } else if (component.type === 'FOOTER') {
+          this.footerValue = component.text;
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.max-h-full {
+  max-height: 60% !important;
+}
+</style>
