916454a502bb8fb646d604e0025e3c8107c46059
shozabnadeem <shozabnadeem1103@gmail.com>
2023-06-26 17:38:18 +0500

    Remove debugger

--- app/javascript/dashboard/routes/dashboard/settings/ecommerece/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/ecommerece/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full">
-    <iframe :src="ecommerece_url" height="100%" width="100%" frameborder="0"></iframe>
+    <iframe :src="ecommerece_url" height="100%" width="100%" frameborder="0" />
   </div>
 </template>
 
@@ -8,21 +8,22 @@
 import md5 from 'md5';
 import { mapGetters } from 'vuex';
 
-
 export default {
   computed: {
     ...mapGetters({
       currentAccountId: 'getCurrentAccountId',
     }),
     ecommerece_url() {
-      let url = window.chatwootConfig.ecommereceUrl + md5(this.currentAccountId.toString()+window.chatwootConfig.randomString)
-      debugger
-      return url 
-    }
+      let url =
+        window.chatwootConfig.ecommereceUrl +
+        md5(
+          this.currentAccountId.toString() + window.chatwootConfig.randomString
+        );
+      return url;
+    },
   },
-}
+};
 </script>
-
 
 <style scoped>
 .h-full {
